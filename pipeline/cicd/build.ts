7a328e1d0e332c14da001a47f461b4b47e799e47
iwaseshi <aaa.1470.333@gmail.com>
2023-02-23 21:41:09 +0900

    feat: #2 init cicd

---
+++ pipeline/cicd/build.ts
@@ -0,0 +1,9 @@
+import Client, { connect } from "@dagger.io/dagger";
+
+connect(async (client: Client) => {
+  const node = client.container().from("node:18").withExec(["node", "-v"]);
+
+  const version = await node.stdout();
+
+  console.log("Hello from Dagger and Node " + version);
+});
